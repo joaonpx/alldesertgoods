c8301db556768081b21369400310e055a01a3512
Npx <cheyprivado@gmail.com>
2023-08-28 21:28:40 -0300

    :hammer: Update main script

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import { updateCart } from "./utils/cart.js"
+
+let addBtns = document.querySelectorAll(".gallery-img-container")
+let cartCount = document.querySelector(".count")
+
+if (!localStorage.count) {
+  localStorage.count = 0
+}
+
+updateCart()
+
+function addItem() {
+  if (localStorage.count) {
+    localStorage.count = Number(localStorage.count) + 1
+  } else {
+    localStorage.count = 0
+  }
+
+  updateCart()
+}
+
+addBtns.forEach((btn) => {
+  btn.addEventListener("click", addItem)
+})
+
+let colorfulBtn = document.querySelector(".colorful")
+
+colorfulBtn.addEventListener("click", () => {
+  document.body.classList.add("colorful")
+})
