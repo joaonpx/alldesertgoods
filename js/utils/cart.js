446e19d70950e04aa72c09f358337be4a92f6adf
Npx <cheyprivado@gmail.com>
2023-08-30 21:59:45 -0300

    :hammer: Update cart script

--- js/utils/cart.js
+++ js/utils/cart.js
@@ -1,5 +1,17 @@
-let cartCount = document.querySelector(".count")
+function updateCart() {
+  let cartCount = document.querySelector(".count")
 
-export function updateCart() {
   cartCount.setAttribute("data-count", localStorage.count)
 }
+
+function addItem() {
+  if (localStorage.count) {
+    localStorage.count = Number(localStorage.count) + 1
+  } else {
+    localStorage.count = 1
+  }
+
+  updateCart()
+}
+
+export { updateCart, addItem }
