fc178fd06d698b3846c58e0f373c2e5ba2e362c7
Npx <cheyprivado@gmail.com>
2023-09-03 21:29:30 -0300

    :hammer: Update main script

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-import { updateCart, addItem } from "./utils/cart.js"
+import { updateCart, openCart, closeCart, addItem } from "./utils/cart.js"
 
 updateCart()
 
 let addBtns = document.querySelectorAll(".gallery-item")
 
 addBtns.forEach((btn) => {
-  btn.addEventListener("click", addItem)
+  btn.addEventListener("click", (event) => {
+    let item = event.currentTarget
+    let itemPrice = item.querySelector("#price").dataset.price
+
+    addItem(itemPrice)
+  })
 })
+
+let openBtn = document.querySelector(".open-btn")
+
+openBtn.addEventListener("click", openCart)
+
+let closeBtn = document.querySelector(".close-btn")
+
+closeBtn.addEventListener("click", closeCart)
 
 let colorfulBtn = document.querySelector(".colorful")
 
