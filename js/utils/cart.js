46836d098351716a74769fd40ef3b74e3b222570
Npx <cheyprivado@gmail.com>
2023-09-03 21:29:18 -0300

    :hammer: Update cart script

--- js/utils/cart.js
+++ js/utils/cart.js
@@ -1,17 +1,47 @@
-function updateCart() {
-  let count = document.querySelector(".count")
-
-  count.setAttribute("data-count", localStorage.cartCount)
+if (!localStorage.cartCount) {
+  localStorage.cartCount = 0
 }
 
-function addItem() {
+if (!localStorage.cartPrice) {
+  localStorage.cartPrice = 0
+}
+
+function updateCart() {
+  document
+    .querySelector(".count")
+    .setAttribute("data-count", localStorage.cartCount)
+}
+
+function openCart() {
+  document.querySelector(".offcanvas").classList.add("active")
+}
+
+function closeCart() {
+  document.querySelector(".offcanvas").classList.remove("active")
+}
+
+function updateCartCount() {
   if (localStorage.cartCount) {
     localStorage.cartCount = Number(localStorage.cartCount) + 1
   } else {
     localStorage.cartCount = 1
   }
+}
+
+function updateCartPrice(price) {
+  if (localStorage.cartPrice) {
+    localStorage.cartPrice = Number(localStorage.cartPrice) + Number(price)
+  } else {
+    localStorage.cartPrice = Number(price)
+  }
+}
+
+function addItem(itemPrice) {
+  updateCartPrice(itemPrice)
+
+  updateCartCount()
 
   updateCart()
 }
 
-export { updateCart, addItem }
+export { updateCart, openCart, closeCart, addItem }
