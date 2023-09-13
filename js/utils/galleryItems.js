1daad89dd85f1913d648fef5a842d993ecd91758
Npx <cheyprivado@gmail.com>
2023-09-13 20:59:58 -0300

    :hammer: Update galleryItems script

--- js/utils/galleryItems.js
+++ js/utils/galleryItems.js
@@ -1,37 +1,39 @@
-export const items = [
+const items = [
   {
     name: "UWD Project #1",
-    img: (src = "./assets/art-gallery/uwd-1.png"),
+    img: "./assets/art-gallery/uwd-1.png",
     price: 5,
   },
 
   {
     name: "UWD Project #2",
-    img: (src = "./assets/art-gallery/uwd-2.png"),
+    img: "./assets/art-gallery/uwd-2.png",
     price: 5,
   },
 
   {
     name: "UWD Project #3",
-    img: (src = "./assets/art-gallery/uwd-3.png"),
+    img: "./assets/art-gallery/uwd-3.png",
     price: 5,
   },
 
   {
     name: "UWD Project #4",
-    img: (src = "./assets/art-gallery/uwd-4.png"),
+    img: "./assets/art-gallery/uwd-4.png",
     price: 10,
   },
 
   {
     name: "UWD Project #5",
-    img: (src = "./assets/art-gallery/uwd-5.png"),
+    img: "./assets/art-gallery/uwd-5.png",
     price: 10,
   },
 
   {
     name: "UWD Project #6",
-    img: (src = "./assets/art-gallery/uwd-6.png"),
+    img: "./assets/art-gallery/uwd-6.png",
     price: 10,
   },
 ]
+
+export { items }
