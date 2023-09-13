2c86945c538020aa7e739e479657490a98dfc1ce
Npx <cheyprivado@gmail.com>
2023-09-13 19:40:28 -0300

    :hammer: Add galleryItems script

---
+++ js/utils/galleryItems.js
@@ -0,0 +1,37 @@
+export const items = [
+  {
+    name: "UWD Project #1",
+    img: (src = "./assets/art-gallery/uwd-1.png"),
+    price: 5,
+  },
+
+  {
+    name: "UWD Project #2",
+    img: (src = "./assets/art-gallery/uwd-2.png"),
+    price: 5,
+  },
+
+  {
+    name: "UWD Project #3",
+    img: (src = "./assets/art-gallery/uwd-3.png"),
+    price: 5,
+  },
+
+  {
+    name: "UWD Project #4",
+    img: (src = "./assets/art-gallery/uwd-4.png"),
+    price: 10,
+  },
+
+  {
+    name: "UWD Project #5",
+    img: (src = "./assets/art-gallery/uwd-5.png"),
+    price: 10,
+  },
+
+  {
+    name: "UWD Project #6",
+    img: (src = "./assets/art-gallery/uwd-6.png"),
+    price: 10,
+  },
+]
