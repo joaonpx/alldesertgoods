81853e107f3972cd88c444515c72b81885db6c58
Npx <cheyprivado@gmail.com>
2023-09-13 20:58:56 -0300

    :hammer: Update main script

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 import { updateCart, openCart, closeCart, addItem } from "./utils/cart.js"
 
+import { items } from "./utils/galleryItems.js"
+
 updateCart()
+
+items.forEach((item) => {
+  let galleryColumnA = document.querySelector(".col-a")
+  let galleryColumnB = document.querySelector(".col-b")
+
+  let itemElement = document.createElement("div")
+  itemElement.className = "gallery-item"
+  itemElement.innerHTML = `<div class="gallery-img-container"><img class="gallery-img" src="${item.img}" alt="Gallery Item 1"/></div><span id="title">${item.name}</span><span id="price" data-price="${item.price}">$ </span>`
+
+  if (items.indexOf(item) <= 2) {
+    galleryColumnA.appendChild(itemElement)
+  } else if (items.indexOf(item) > 2 && items.indexOf(item) <= 6) {
+    galleryColumnB.appendChild(itemElement)
+  }
+})
 
 let addButtons = document.querySelectorAll(".gallery-item")
 
